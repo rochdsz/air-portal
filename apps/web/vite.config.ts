bc351ab63c3908cf6b03c52c8e009e6a2724198b
rochelledsz <Rhelledsou@gmail.com>
2026-02-05 19:23:04 +0530

    Fix production crash using vite-plugin-node-polyfills

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'; // <--- Import this
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `global` variable
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@air-portal/shared": path.resolve(__dirname, "../../packages/shared/index.ts"),
     },
-  },
-  define: {
-    global: 'window',
   },
   server: {
     fs: {
